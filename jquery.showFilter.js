96d0d8bbe846fef31eb819186a6e71b7d3600008
Peter West <peterjwest3@gmail.com>
2010-10-25 20:46:32 +0100

    now filters  a single input with criteria

--- jquery.showFilter.js
+++ jquery.showFilter.js
@@ -1,11 +1,12 @@
-(function( $ ){
+(function($){
 	$.fn.showFilter = function(selectors, criteria, options) {
 		this.data("lastVal",this.val());
-		var search = this.val();
+		var input = this;
 		var test = function() {
 			var c = criteria($(this));
 			var matched = false;
-			for (i in c) if (c[i].toLower.indexOf(search.toLower) > -1) matched = true;
+			var terms = $.trim(input.val()).toLowerCase().split(/\s+/);
+			for (i in c) for (j in terms) if (c[i].toLowerCase().indexOf(terms[j]) > -1) matched = true;
 			if (matched) $(this).show();
 			else $(this).hide();
 		}
@@ -16,4 +17,4 @@
 		}
 		this.bind("keyup keydown change paste", filter);
 	};
-})( jQuery );
+})(jQuery);
