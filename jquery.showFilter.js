daed73d2d31efd71666a4460dd6b9434f61ba79b
Pete <peterjwest3@gmail.com>
2010-10-27 00:36:16 +0100

    added support for multiple, overlapping filters

--- jquery.showFilter.js
+++ jquery.showFilter.js
@@ -1,10 +1,17 @@
 (function($){
 	$.fn.showFilter = function(selectors, criteria, options) {
 		this.data("lastVal", this.val());
+		var id = 1;
+		while($(".showFilter"+id).length > 0) id++;
+		this.addClass("showFilter"+id);
 		var input = this;
 		var filterInput = function(input) { return $.trim(input).toLowerCase().replace(/\s+/, " ") }
 		var test = function() {
 			var c = criteria($(this));
+			var filters = $(this).data("showFilter");
+			if (!filters) filters = $(this).data("showFilter", {}).data("showFilter");
+			var filtered = false;
+			for (i in filters) if (filters[i] && i != id) filtered = true;
 			var terms = filterInput(input.val()).split(/\s+/);
 			var matched = false;
 			for (i in c) {
@@ -12,12 +19,17 @@
 				for (j in terms) if (!(c[i].toLowerCase().indexOf(terms[j]) > -1)) matches = false;
 				if (matches) matched = true;
 			}
-			if (matched) $(this).show();
-			else $(this).hide();
+			if (matched) {
+				filters[id] = false;
+				if (!filtered) $(this).show();
+			}
+			else {
+				filters[id] = true;
+				if (!filtered) $(this).hide();
+			}
 		}
 		var filter = function() {
 			if (filterInput($(this).val()) === filterInput($(this).data("lastVal"))) return;
-			alert();
 			$(this).data("lastVal", $(this).val());
 			$(selectors).each(test);
 		}
