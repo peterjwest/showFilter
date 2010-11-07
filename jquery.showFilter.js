e50c52de931fcdd8058e9cd71bad19a0287b7b8a
Peter West <peterjwest3@gmail.com>
2010-11-07 15:17:30 +0000

    Added jquery options, fix for multiple criteria

--- jquery.showFilter.js
+++ jquery.showFilter.js
@@ -1,23 +1,41 @@
 (function($){
 	$.fn.showFilter = function(selectors, criteria, options) {
+		var settings = {
+			filterClass: 'showFilter', 
+			filter: function(e) { e.hide(); }, 
+			unfilter: function(e) { e.show(); }
+		};
+		if (options) $.extend(settings, options);
 		this.data("lastVal", this.val());
+		var id = 1;
+		while($("."+settings.filterClass+id).length > 0) id++;
+		this.addClass(settings.filterClass+id);
 		var input = this;
-		var filterInput = function(input) { return $.trim(input).toLowerCase().replace(/\s+/, " ") }
+		var filterInput = function(input) { return $.trim(input.toLowerCase().replace(/[,\s]+/, " ")); }
 		var test = function() {
-			var c = criteria($(this));
-			var terms = filterInput(input.val()).split(/\s+/);
-			var matched = false;
-			for (i in c) {
-				var matches = true;
-				for (j in terms) if (!(c[i].toLowerCase().indexOf(terms[j]) > -1)) matches = false;
-				if (matches) matched = true;
+			var filters = $(this).data("showFilter");
+			if (!filters) filters = $(this).data("showFilter", {}).data("showFilter");
+			var filtered = false;
+			for (i in filters) if (filters[i] && i != id) filtered = true;
+			var terms = filterInput(input.val()).split(/[,\s]+/);
+			var matched = true;
+			var criterias = criteria($(this));
+			for (j in terms) {
+				var matches = false;
+				for (i in criterias) if ((criterias[i].toLowerCase().indexOf(terms[j]) > -1)) matches = true;
+				if (!matches) matched = false;
 			}
-			if (matched) $(this).show();
-			else $(this).hide();
+			if (matched) {
+				filters[id] = false;
+				if (!filtered) settings.unfilter($(this));
+			}
+			else {
+				filters[id] = true;
+				if (!filtered) settings.filter($(this));
+			}
 		}
 		var filter = function() {
 			if (filterInput($(this).val()) === filterInput($(this).data("lastVal"))) return;
-			alert();
 			$(this).data("lastVal", $(this).val());
 			$(selectors).each(test);
 		}
